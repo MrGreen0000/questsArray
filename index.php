6317ca6efbd6a9d7ed30c4f70a267bae5be74179
MrGreen0000 <91195806+MrGreen0000@users.noreply.github.com>
2022-03-30 21:43:38 +0200

    update undex.php

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 # questsArray
-
 <?php
 
 $moviesIndiana = [ 
-    'Indiana Jones et la dernière croisade '=> 'Indina Jones',
-    'Indiana Jones et le Temple maudit' => 'Indina Jones',
-    "Les Aventuriers de l'arche perdue" => 'Indina Jones',
+    'Indiana Jones et la dernière croisade '=> ['Harrison Ford', 'Karen Alen', 'Paul Freeman'],
+    'Indiana Jones et le Temple maudit' => ['Harrison Ford', 'Kate cabshaw', 'Ke Huy Quan'],
+    "Les Aventuriers de l'arche perdue" => ['Harrison Ford', 'Sean Connery', 'Alison Doody'],
     ];
-    foreach($moviesIndiana as $movie => $actor) {
-        echo  $actor. ": " . " " .$movie . PHP_EOL;
-    } 
-      
+    
+
+    foreach ($moviesIndiana as $movie =>list($firstActor, $secondActor, $thirdActor )) {
+        
+        echo "Dans le film". " " . $movie . ", les principaux acteurs sont : \n". " $firstActor\n $secondActor\n $thirdActor\n";
+    
+    }
+    
 
 ?>
