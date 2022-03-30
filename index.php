70023e9aad31e5021dab7f1bd8ac2faa76524182
MrGreen0000 <91195806+MrGreen0000@users.noreply.github.com>
2022-03-30 21:00:29 +0200

    challenge quest array multidimensional

---
+++ index.php
@@ -0,0 +1,15 @@
+# questsArray
+
+<?php
+
+$moviesIndiana = [ 
+    'Indiana Jones et la dernière croisade '=> 'Indina Jones',
+    'Indiana Jones et le Temple maudit' => 'Indina Jones',
+    "Les Aventuriers de l'arche perdue" => 'Indina Jones',
+    ];
+    foreach($moviesIndiana as $movie => $actor) {
+        echo  $actor. ": " . " " .$movie . PHP_EOL;
+    } 
+      
+
+?>
